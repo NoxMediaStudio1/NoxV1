835e4f6b34f0bcf0c438aca168cac0cbdca84fa2
Philippe <noxsubph13@gmail.com>
2025-07-31 11:50:16 -0300

    Subindo os arquivos iniciais do projeto5

--- index.tsx
+++ index.tsx
@@ -1,15 +1,8 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './i18n'; // Updated import path
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
